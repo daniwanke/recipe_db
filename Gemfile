76224792e95d6890c697147ec13ea41fa67bd108
daniwanke <daniwanke@DESKTOP-0UQIEHC.localdomain>
2020-03-03 15:27:11 -0600

    added pg for heroku

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem "cocoon"
 gem "devise"
 
 group :development, :test do
+    gem 'sqlite3', '~> 1.4'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
@@ -44,7 +45,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'sqlite3', '~> 1.4'
 end
 
 group :test do
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -67,8 +67,8 @@ class RecipesController < ApplicationController
       @recipe = Recipe.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Only allow a list of trusted parameters through.
     def recipe_params
-      params.require(:recipe).permit(:remove_image, :image, :title, :description, ingredients_attributes:[:id, :content, :_destroy], steps_attributes:[:id, :direction, :_destroy])
-    end
+params.require(:recipe).permit(:remove_image, :image, :title, :description, ingredients_attributes:[:id, :content, :_destroy], steps_attributes:[:id, :direction, :_destroy])
+end
 end
