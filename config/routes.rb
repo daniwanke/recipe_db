47f83ade12a8b6331127b3e46894ff472cf2c766
daniwanke <daniwanke@DESKTOP-0UQIEHC.localdomain>
2020-03-04 10:16:49 -0600

    remove devise again

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :installs
   resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root 'recipes#index'
